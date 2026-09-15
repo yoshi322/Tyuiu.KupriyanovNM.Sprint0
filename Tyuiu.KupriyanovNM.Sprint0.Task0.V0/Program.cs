a5775d9a8b1c8ad3a67f1f299ff76b6ef29c3bb2
yoshimura993 <kuprijnovaev2008@gmail.com>
2026-09-15 18:13:50 +0500

    Add project files.

---
+++ Tyuiu.KupriyanovNM.Sprint0.Task0.V0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.KupriyanovNM.Sprint0.Task0.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
