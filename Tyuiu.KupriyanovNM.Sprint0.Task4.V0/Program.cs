15fa1075263c465bca927e41ddfa82c9aae61d1a
yoshimura993 <kuprijnovaev2008@gmail.com>
2026-09-15 23:18:35 +0500

    Task4

--- Tyuiu.KupriyanovNM.Sprint0.Task4.V0/Program.cs
+++ Tyuiu.KupriyanovNM.Sprint0.Task4.V0/Program.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.KupriyanovNM.Sprint0.Task4.V0
             Console.WriteLine(DataService.Addition(1, 5));
             Console.WriteLine(DataService.Subtraction(15, 5));
             Console.WriteLine(DataService.Multiplication(10, 10));
-            Console.WriteLine(DataService.Division(5, 5));
+            Console.WriteLine(DataService.Division(10, 5));
             Console.ReadKey();
         }
     }
