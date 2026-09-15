fd91fa46840840267d733a5d19defed9431ff87e
yoshimura993 <kuprijnovaev2008@gmail.com>
2026-09-15 22:52:56 +0500

    Task3

---
+++ Tyuiu.KupriyanovNM.Sprint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.KupriyanovNM.Sprint0.Task3.V0.Lib
+{
+    public class DataService
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Tyuiu.KupriyanovNM.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.KupriyanovNM.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.KupriyanovNM.Sprint0.Task3.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();
+        }
+    }
+}
