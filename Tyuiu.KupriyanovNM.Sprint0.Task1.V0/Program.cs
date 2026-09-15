54721686255889dedb456276d6bd784fb3b9c9b4
yoshimura993 <kuprijnovaev2008@gmail.com>
2026-09-15 22:15:32 +0500

    Task1

---
+++ Tyuiu.KupriyanovNM.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.KupriyanovNM.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "никита";
+            int age = 18;
+            Console.WriteLine("здравствуйте меня зовут " + name + " мне " + age + " лет");
+            Console.ReadKey();
+        }
+    }
+}
