c9f2b431b614cd257829c2a3f99f9801bee9f189
yoshimura993 <kuprijnovaev2008@gmail.com>
2026-09-16 22:47:35 +0500

    Task6

--- Tyuiu.KupriyanovNM.Sprint0.Task6.V0/Program.cs
+++ Tyuiu.KupriyanovNM.Sprint0.Task6.V0/Program.cs
@@ -5,7 +5,7 @@ namespace Tyuiu.KupriyanovNM.Sprint0.Task6.V0
     {
         static void Main(string[] args)
         {
-            int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
+            int[] numsArray = new int[] { 1, 2, 3, 4, 5};
             Console.WriteLine("сумма элементов массива = " + DataService.AdditionArray(numsArray));
             Console.WriteLine("разность элементов массива = " + DataService.SubtractionArray(numsArray));
             Console.WriteLine("произведение элементов массива = " + DataService.MultiplicationArray(numsArray));
